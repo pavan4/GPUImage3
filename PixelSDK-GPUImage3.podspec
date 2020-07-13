7fbd6ac892054ceca124d259ce2766b77de82129
Josh Bernfeld <joshbernfeld@gmail.com>
2020-07-13 14:10:36 -0700

    Add WhiteBalance support

--- PixelSDK-GPUImage3.podspec
+++ PixelSDK-GPUImage3.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
     s.name     = 'PixelSDK-GPUImage3'
     s.module_name = 'GPUImage'
-    s.version  = '1.0.5'
+    s.version  = '1.1.0'
     s.license  = 'BSD'
     s.summary  = 'An open source iOS framework for GPU-based image and video processing.'
     s.homepage = 'https://github.com/GottaYotta/GPUImage3'
@@ -9,8 +9,7 @@ Pod::Spec.new do |s|
     s.source   = { :git => 'https://github.com/GottaYotta/GPUImage3.git', :tag => s.version }
 
     s.source_files = 'framework/Source/**/*.{swift,h,metal}'
-    s.public_header_files = 'framework/Source/Empty.h'
-    s.exclude_files = ['framework/Source/Operations/WhiteBalance.{swift,metal}']
+    s.public_header_files = ''
 
     s.ios.deployment_target = '9.0'
     s.frameworks   = ['Metal', 'QuartzCore', 'AVFoundation']
--- framework/Source/Operations/WhiteBalance.swift
+++ framework/Source/Operations/WhiteBalance.swift
@@ -1,4 +1,3 @@
-/*
 public class WhiteBalance: BasicOperation {
     public var temperature:Float = 5000.0 { didSet { uniformSettings["temperature"] = temperature < 5000.0 ? 0.0004 * (temperature - 5000.0) : 0.00006 * (temperature - 5000.0) } }
     public var tint:Float = 0.0 { didSet { uniformSettings["tint"] = tint / 100.0 } }
@@ -6,8 +5,10 @@ public class WhiteBalance: BasicOperation {
     public init() {
         super.init(fragmentFunctionName:"whiteBalanceFragmentShader", numberOfInputs:1)
         
-        uniformSettings.appendUniform(5000.0)
-        uniformSettings.appendUniform(0.0)
+        ({temperature = 5000.0})()
+        ({tint = 0.0})()
+        
+        uniformSettings["RGBtoYIQ"] = Matrix3x3(rowMajorValues: [0.299, 0.587, 0.114, 0.596, -0.274, -0.322, 0.212, -0.523, 0.311])
+        uniformSettings["YIQtoRGB"] = Matrix3x3(rowMajorValues: [1.0, 0.956, 0.621, 1.0, -0.272, -0.647, 1.0, -1.105, 1.702])
     }
 }
- */
